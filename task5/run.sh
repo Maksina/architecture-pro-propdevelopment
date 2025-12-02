93034081b150cc4a9a74f7576d7b691b59f7be49
Maksina <m.schurenkov@gmail.com>
2025-12-02 20:09:43 +0300

    fix task5

---
+++ task5/run.sh
@@ -0,0 +1,14 @@
+echo "Запуск сервисов..."
+
+kubectl run front-end-app --image=nginx --labels role=front-end --expose --port=80
+kubectl run back-end-api-app --image=nginx --labels role=back-end-api --expose --port=80
+kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port=80
+kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --expose --port=80
+
+echo "Сервисы запущены"
+
+echo "Применение сетевых политик"
+kubectl apply -f NetworkPolicy.yaml
+
+echo "Сетевые политики применены"
+read -p "Нажмите Enter для завершения..."
